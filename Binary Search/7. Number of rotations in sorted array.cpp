8c5dcd2c527c60250c6e06e15e6d32b864583a61
Nikhil Patel <nikhilid2205@gmail.com>
2023-01-28 16:39:20 +0530

    Count number of rotations.cpp

---
+++ Binary Search/7. Number of rotations in sorted array.cpp
@@ -0,0 +1,45 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+int count(vector<int> &arr, int n)
+{
+    if(n==1) return 0;
+    if(arr[0]<arr[n-1]) return 0;
+
+    int start=0;
+    int end = n-1;
+    int mid = start + (end-start)/2;
+    
+
+      while(start<=end){
+            int mid=start+(end-start)/2;
+            int next=(mid+1)%n;
+            int prev=(mid-1+n)%n;
+            if((arr[mid]<arr[next]) &&(arr[mid]<arr[prev])){
+                return mid;
+            }
+            else if(arr[start]<=arr[mid] && arr[mid]<=arr[end]){
+                end=mid-1;
+            }
+            else{
+                if(arr[start]<=arr[mid]){
+                start=mid+1;
+                }
+                else if(arr[mid]<=arr[end]){
+                end=mid-1;
+            }
+            }
+             
+        }
+        return 0;
+}
+int main()
+{
+    int n; cin>>n;
+    vector<int> v(n);
+    for(int i=0;i<n;i++) cin>>v[i];
+
+    // array must be sorted;
+
+    cout << count(v,n);
+}
