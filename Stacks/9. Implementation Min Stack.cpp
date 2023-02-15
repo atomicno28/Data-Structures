277f2468b3df998526b5d053be5ada010bc8df4d
Nikhil Patel <nikhilid2205@gmail.com>
2023-02-15 13:32:12 +0530

    With Extra Space

---
+++ Stacks/9. Implementation Min Stack.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+#include<vector>
+#include<stack>
+using namespace std;
+stack<int>s;
+stack<int>ss;
+void push(int a)
+{
+    s.push(a);
+    if(ss.empty() || a>ss.top()) ss.push(a);
+}
+int pop()
+{
+    if(s.empty()) return -1;
+    int tmp = s.top();
+    s.pop();
+    if(tmp==ss.top()) ss.pop();
+    return tmp;
+}
+int getMin()
+{
+    if(ss.empty()) return -1;
+    return ss.top();
+}
+int main()
+{
+    int n; cin>>n;
+    vector<int> v(n);
+    
+    for(int i=0;i<n;i++) cin>>v[i];
+    
+    for(int i=0;i<n;i++)
+    {
+        push(v[i]);
+    }
+}
