b8becefcf2d08764da083c71817fa2a6d9add541
Nikhil Patel <nikhilid2205@gmail.com>
2023-02-16 14:49:42 +0530

    Largest-Min, Smallest - Max

---
+++ Heap/1. Kth Smallest-Largest.cpp
@@ -0,0 +1,43 @@
+#include<iostream>
+
+// Header file of PriorityQueue.
+#include<queue>
+
+using namespace std;
+
+int KthSmallest(vector<int>&v, int target)
+{
+   priority_queue<int> maxHeap;
+   
+   for(auto i:v)
+   {
+      maxHeap.push(i);
+      if(maxHeap.size()>target) maxHeap.pop();
+   }
+   return maxHeap.top();
+}
+
+int KthLargest(vector<int>&v, int target)
+{
+      priority_queue<int,vector<int>,greater<int>> minHeap;
+
+      for(auto i:v)
+      {
+         minHeap.push(i);
+         if(minHeap.size()>target) minHeap.pop();
+      }
+      return minHeap.top();
+}
+int main()
+{
+   int n; cin>>n;
+   vector<int> v(n);
+
+   for(int i=0;i<n;i++) cin>>v[i];
+
+   int target; cin>>target;
+
+   cout << KthSmallest(v,target);
+
+   cout<< KthLargest(v,target);
+}
