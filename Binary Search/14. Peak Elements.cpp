c371591be5d61abff7eea4b2421ef635ad18028f
Nikhil Patel <nikhilid2205@gmail.com>
2023-02-26 16:58:22 +0530

    Peak Elements

---
+++ Binary Search/14. Peak Elements.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+int findPeakElement(vector<int>& nums) 
+{
+        if(nums.size()==1) return 0;
+        int start=0,end=nums.size()-1;
+        while(start<=end)
+        {
+            int mid= start + (end-start)/2;
+            if(mid>0 && mid<nums.size()-1)
+            {
+                if(nums[mid]>nums[mid-1] && nums[mid]>nums[mid+1]) return mid;
+                else if(nums[mid-1]>nums[mid]) end = mid-1;
+                else start=mid+1;
+            }
+            else if(mid==0)
+            {
+                return (nums[0]>nums[1])?0:1;
+            }
+            else if(mid==nums.size()-1)
+            {
+                return (nums[nums.size()-1]>nums[nums.size()-2])? nums.size()-1:nums.size()-2;
+            }
+        }
+        return -1;
+}
+int main()
+{
+    int n; cin>>n;
+    vector<int> v(n);
+    
+    for(int i=0;i<n;i++) cin>>v[i];
+    
+    cout << findPeakElement(v);
+}
