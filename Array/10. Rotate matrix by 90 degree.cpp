c50c72c4dfd5e64a69b585ad6bdc66bbd1184e82
Nikhil Patel <nikhilid2205@gmail.com>
2023-03-31 22:47:51 +0530

    swap and reverse.

---
+++ Array/10. Rotate matrix by 90 degree.cpp
@@ -0,0 +1,40 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+int main()
+{
+    int m,n;
+    cin>>m>>n;
+
+    vector<vector<int>> arr(m,vector<int>(n));
+
+    for(int i=0;i<m;i++)
+    {
+        for(int j=0;j<n;j++)
+        {
+            cin>>arr[i][j];
+        }
+    }
+
+    for(int i=0;i<m;i++)
+    {
+        for(int j=0;j<i;j++)
+        {
+            swap(arr[i][j],arr[j][i]);
+        }
+    }
+
+    for(int i=0;i<m;i++)
+    {
+        reverse(arr[i].begin(),arr[i].end());
+    }
+
+    for(int i=0;i<m;i++)
+    {
+        for(int j=0;j<n;j++)
+        {
+            cout<<arr[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+}
