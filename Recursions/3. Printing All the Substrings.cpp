474557cfa2e44f436e345d3affbef6297fa04ca0
Nikhil Patel <nikhilid2205@gmail.com>
2023-04-07 11:55:25 +0530

    String

---
+++ Recursions/3. Printing All the Substrings.cpp
@@ -0,0 +1,31 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+void subsq(string s, int i, int n, string&temp)
+{
+    if(i==n)
+    {
+        for(int i=0;i<temp.size();i++) cout<<temp[i];
+        cout<<endl;
+        return;
+    }
+    
+   //pick
+   temp+=s[i];
+    
+    subsq(s,i+1,n,temp);
+    
+    //notpick
+    temp.pop_back();
+    
+    subsq(s,i+1,n,temp);
+}
+int main()
+{
+   string s;
+   cin>>s;
+   
+   string temp="";
+   
+   subsq(s,0,s.size(),temp);
+}
