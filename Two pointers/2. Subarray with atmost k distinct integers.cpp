cd39289f0f6760c8599db87106f00a09521570a6
Nikhil Patel <nikhilid2205@gmail.com>
2023-05-06 16:44:35 +0530

    Atmost Approach.

---
+++ Two pointers/2. Subarray with atmost k distinct integers.cpp
@@ -0,0 +1,38 @@
+#include<iostream>
+#include<vector>
+#include<map>
+using namespace std;
+int main()
+{
+    int n; cin>>n;
+    vector<int> v(n);
+
+    for(int i=0;i<n;i++) cin>>v[i];
+
+    int k; cin>>k;
+
+    int ans =0;
+    int i=0,j=0;
+
+    map<int,int> mp;
+
+    while(j<n)
+    {
+        
+        mp[v[j]]++;
+
+        // if map size exceeds K.
+        while(mp.size()>k)
+        {
+            mp[v[i]]--;
+            if(mp[v[i]]==0) mp.erase(v[i]);
+            i++;
+        }
+
+        // store the size of window.
+        ans += (j-i+1);
+        j++;
+    }
+
+
+}
