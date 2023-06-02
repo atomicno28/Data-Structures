3fd6bb651c651e1e4140ce9bc26697198cc4185d
Nikhil Patel <nikhilid2205@gmail.com>
2023-06-02 19:47:19 +0530

    stilll pending.............

---
+++ Binary Search/Search Space/2. Find kth element in two sorted arrays.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+int main()
+{
+    int n,m; cin>>n>>m;
+
+    vector<int> v1(n);
+    vector<int> v2(m);
+
+    for(int i=0;i<n;i++) cin>>v1[i];
+    for(int i=0;i<m;i++) cin>>v2[i];
+
+    int k; cin>>k;
+
+    
+
+
+    
+}
