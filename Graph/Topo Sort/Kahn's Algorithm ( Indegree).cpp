4fa82596481c2e0e2774f0685c7d346cde77c986
Nikhil Patel <nikhilid2205@gmail.com>
2023-07-07 15:28:30 +0530

    BFS

---
+++ Graph/Topo Sort/Kahn's Algorithm ( Indegree).cpp
@@ -0,0 +1,45 @@
+#include<unordered_map>
+#include<vector>
+#include<queue>
+using namespace std;
+class Solution
+{
+	public:
+	//Function to return list containing vertices in Topological order. 
+	vector<int> topoSort(int V, vector<int> adj[]) 
+	{
+	    unordered_map<int,int> indegree;
+	    
+	    // setting the indegrees
+	    for(int i=0;i<V;i++)
+	    {
+	        for(int j=0;j<adj[i].size();j++)
+	        {
+	            indegree[adj[i][j]]++;
+	        }
+	    }
+	    
+	    queue<int> q;
+	    
+	    for(int i=0;i<V;i++)
+	    {
+	        if(!indegree[i]) q.push(i);
+	    }
+	    
+	    vector<int> ans;
+	    
+	    while(!q.empty())
+	    {
+	        int front = q.front();
+	        q.pop();
+	        
+	        ans.push_back(front);
+	        for(auto i:adj[front])
+	        {
+	            indegree[i]--;
+	            if(indegree[i]==0) q.push(i);
+	        }
+	    }
+	    return ans;
+	}
+};
