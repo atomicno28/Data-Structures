d613b0947a436551519997fc85d1a375c5aa2838
Nikhil Patel <nikhilid2205@gmail.com>
2023-09-14 19:41:20 +0530

    Traversal + Count

---
+++ Graph/6. Number of Connected Components.cpp
@@ -0,0 +1,17 @@
+/// IT IS PREETY SIMPLE
+#include<iostream>
+using namespace std;
+void travers()
+{
+
+}
+int main()
+{
+    int visited[9];
+    int count = 0;
+ for(int i=0;i<9;i++)
+ {
+    if(!visited[i])
+        count++,
+        travers();
+}
