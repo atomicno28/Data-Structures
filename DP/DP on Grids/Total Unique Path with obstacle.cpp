5bcf3e15dd4e5e9279429ec48ca96ce6a9d19b9a
Nikhil Patel <72176039+atomicno28@users.noreply.github.com>
2024-06-08 03:34:13 +0530

    Update Total Unique Path with obstacle.cpp

--- DP/DP on Grids/Total Unique Path with obstacle.cpp
+++ DP/DP on Grids/Total Unique Path with obstacle.cpp
@@ -39,3 +39,25 @@ int main()
    
 
 }
+
+
+//// memoisation approach
+ int ans(int i, int j, vector<vector<int>>&dp, vector<vector<int>>&grid)
+    {
+        if(i == 0 && j == 0 && !grid[i][j]) return 1;
+
+        if( i < 0 || j < 0 || grid[i][j]) return 0;
+
+        if(dp[i][j] != -1) return dp[i][j];
+
+        int left = ans(i - 1 , j , dp , grid);
+        int right = ans(i , j - 1 , dp , grid);
+
+        return dp[i][j] = left + right; 
+    }
+    int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
+        int m = obstacleGrid.size();
+        int n = obstacleGrid[0].size();
+        vector<vector<int>> dp(m,vector<int>(n,-1));
+        return ans(m-1,n-1,dp,obstacleGrid);
+    }
